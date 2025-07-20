0bfbc07e970349b7e6cf0846daf93ce2de75e295
SRoy <subhodeeproy89@gmail.com>
2025-07-20 12:54:59 +0000

    Update get access token.bat

--- get access token.bat
+++ get access token.bat
@@ -3,7 +3,7 @@ pause
 
 curl -X GET "https://api.developer.atomberg-iot.com/v1/get_access_token" ^
  -H "accept: application/json" ^
- -H "x-api-key: REPLACE" ^
- -H "authorization: Bearer REPLACE"
+ -H "x-api-key: API_KEY_HERE" ^
+ -H "authorization: Bearer REFRESH_TOKEN_HERE"
 
 pause
